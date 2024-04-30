25cfca82a94c5714fce5aa53301cf0f6b0eeddbf
aliensen36 <142628769+aliensen36@users.noreply.github.com>
2024-04-30 22:00:11 +0300

    Move .gitignor.

--- Points_of_view/points/models.py
+++ Points_of_view/points/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Team(models.Model):
     photo = models.ImageField(upload_to='team', verbose_name='Фото')
-    # photo = models.ImageField(upload_to='team_photos/', verbose_name='Фото')
     name = models.CharField(max_length=200, verbose_name='Имя')
     role = models.CharField(max_length=1000, verbose_name='Роль в команде')
 
@@ -12,4 +11,18 @@ class Team(models.Model):
     class Meta:
         verbose_name_plural = 'Члены команды'
         verbose_name = 'Команда'
+        ordering = ['name']
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    photo = models.ImageField(upload_to='project', verbose_name='Фото')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Проекты'
+        verbose_name = 'Проект'
         ordering = ['name']
