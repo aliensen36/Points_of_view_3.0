4f477ee7429a4c0581d71bb15bf9e47c796ddbdf
aliensen36 <142628769+aliensen36@users.noreply.github.com>
2024-05-11 14:56:14 +0300

    Change the presentation of the home page.

--- Points_of_view/points/views.py
+++ Points_of_view/points/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import *
+
 
 def index(request):
-    return render(request, 'index.html')
+    teams = Team.objects.all()
+    return render(request, 'index.html', {'teams': teams})
 
 def art_cartel(request):
     return render(request, 'art-cartel.html')
