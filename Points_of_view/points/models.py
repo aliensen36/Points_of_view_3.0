78a19205273b9d8f750d5b4b4081561a4a8d5660
aliensen36 <142628769+aliensen36@users.noreply.github.com>
2024-05-14 19:17:47 +0300

    Translation of NQ from the database.

--- Points_of_view/points/models.py
+++ Points_of_view/points/models.py
@@ -19,13 +19,39 @@ class Art_diving(models.Model):
     name = models.CharField(max_length=200, verbose_name='Название')
     description = models.TextField(verbose_name='Описание')
     about_title = models.TextField(verbose_name='О проекте(заголовок)')
+    about_b1 = models.TextField(verbose_name='О проекте(бл. 1)')
+    about_b2 = models.TextField(verbose_name='О проекте(бл. 2)')
+    about_b3 = models.TextField(verbose_name='О проекте(бл. 3)')
     about_1 = models.TextField(verbose_name='О проекте(абз. 1)')
     about_2 = models.TextField(verbose_name='О проекте(абз. 2)')
     about_3 = models.TextField(verbose_name='О проекте(абз. 3)')
     details_title = models.TextField(verbose_name='Детали(заголовок)')
+    details_b1 = models.TextField(verbose_name='Детали(бл. 1)')
+    details_b2 = models.TextField(verbose_name='Детали(бл. 2)')
+    details_b3 = models.TextField(verbose_name='Детали(бл. 3)')
     details_1 = models.TextField(verbose_name='Детали(абз. 1)')
     details_2 = models.TextField(verbose_name='Детали(абз. 2)')
     details_3 = models.TextField(verbose_name='Детали(абз. 3)')
+    we_can = models.TextField(verbose_name='Умеем')
+    we_can_1_title = models.TextField(verbose_name='Умеем_1(заг.)')
+    we_can_1 = models.TextField(verbose_name='Умеем_1')
+    we_can_2_title = models.TextField(verbose_name='Умеем_2(заг.)')
+    we_can_2 = models.TextField(verbose_name='Умеем_2')
+    we_can_3_title = models.TextField(verbose_name='Умеем_3(заг.)')
+    we_can_3 = models.TextField(verbose_name='Умеем_3')
+    we_can_4_title = models.TextField(verbose_name='Умеем_4(заг.)')
+    we_can_4 = models.TextField(verbose_name='Умеем_4')
+    impl_header = models.TextField(verbose_name='Реализация')
+    impl_title_1 = models.TextField(verbose_name='Реализация(заг.1)')
+    impl_ext_1 = models.TextField(verbose_name='Реализация(инф.1)')
+    impl_title_2 = models.TextField(verbose_name='Реализация(заг.2)')
+    impl_ext_2 = models.TextField(verbose_name='Реализация(инф.2)')
+    impl_title_3 = models.TextField(verbose_name='Реализация(заг.3)')
+    impl_ext_3 = models.TextField(verbose_name='Реализация(инф.3)')
+    impl_title_4 = models.TextField(verbose_name='Реализация(заг.4)')
+    impl_ext_4 = models.TextField(verbose_name='Реализация(инф.4)')
+    impl_title_5 = models.TextField(verbose_name='Реализация(заг.5)')
+    impl_ext_5 = models.TextField(verbose_name='Реализация(инф.5)')
 
     def __str__(self):
         return self.name
@@ -33,3 +59,34 @@ class Art_diving(models.Model):
     class Meta:
         verbose_name_plural = 'Арт-дивинг'
         verbose_name = 'Арт-дивинг'
+
+
+class Naive_questions(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    about_title = models.TextField(verbose_name='О проекте(заголовок)')
+    about_1 = models.TextField(verbose_name='О проекте(абз. 1)')
+    about_2 = models.TextField(verbose_name='О проекте(абз. 2)')
+    details_title = models.TextField(verbose_name='Детали(заголовок)')
+    details_1 = models.TextField(verbose_name='Детали(абз. 1)')
+    we_can = models.TextField(verbose_name='Умеем')
+    we_can_1_title = models.TextField(verbose_name='Умеем_1(заг.)')
+    we_can_1 = models.TextField(verbose_name='Умеем_1')
+    we_can_2_title = models.TextField(verbose_name='Умеем_2(заг.)')
+    we_can_2 = models.TextField(verbose_name='Умеем_2')
+    we_can_3_title = models.TextField(verbose_name='Умеем_3(заг.)')
+    we_can_3 = models.TextField(verbose_name='Умеем_3')
+    impl_header = models.TextField(verbose_name='Реализация')
+    impl_title_1 = models.TextField(verbose_name='Реализация(заг.1)')
+    impl_ext_1 = models.TextField(verbose_name='Реализация(инф.1)')
+    impl_title_2 = models.TextField(verbose_name='Реализация(заг.2)')
+    impl_ext_2 = models.TextField(verbose_name='Реализация(инф.2)')
+    impl_title_3 = models.TextField(verbose_name='Реализация(заг.3)')
+    impl_ext_3 = models.TextField(verbose_name='Реализация(инф.3)')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Наивные вопросы'
+        verbose_name = 'Наивные вопросы'
--- Points_of_view/points/views.py
+++ Points_of_view/points/views.py
@@ -12,19 +12,88 @@ def index(request):
             'description': diving.description,
         }
     }
-    return render(request, 'index.html', diving_data)
+    nq = Naive_questions.objects.get()
+    nq_data = {
+        'nq_data': {
+            'name': nq.name,
+            'description': nq.description,
+        }
+    }
+    context = {**diving_data, **nq_data}
+    return render(request, 'index.html', context)
 
 def art_cartel(request):
     return render(request, 'art-cartel.html')
 
 def art_diving(request):
-    return render(request, 'art-diving.html')
+    diving = Art_diving.objects.get()
+    diving_data = {'about_title': diving.about_title,
+                   'about_b1': diving.about_b1,
+                   'about_b2': diving.about_b2,
+                   'about_b3': diving.about_b3,
+                   'about_1': diving.about_1,
+                   'about_2': diving.about_2,
+                   'about_3': diving.about_3,
+                   'details_title': diving.details_title,
+                   'details_b1': diving.details_b1,
+                   'details_b2': diving.details_b2,
+                   'details_b3': diving.details_b3,
+                   'details_1': diving.details_1,
+                   'details_2': diving.details_2,
+                   'details_3': diving.details_3,
+                   'we_can': diving.we_can,
+                   'we_can_1_title': diving.we_can_1_title,
+                   'we_can_1': diving.we_can_1,
+                   'we_can_2_title': diving.we_can_2_title,
+                   'we_can_2': diving.we_can_2,
+                   'we_can_3_title': diving.we_can_3_title,
+                   'we_can_3': diving.we_can_3,
+                   'we_can_4_title': diving.we_can_4_title,
+                   'we_can_4': diving.we_can_4,
+                   'impl_header': diving.impl_header,
+                   'impl_title_1': diving.impl_title_1,
+                   'impl_ext_1': diving.impl_ext_1,
+                   'impl_title_2': diving.impl_title_2,
+                   'impl_ext_2': diving.impl_ext_2,
+                   'impl_title_3': diving.impl_title_3,
+                   'impl_ext_3': diving.impl_ext_3,
+                   'impl_title_4': diving.impl_title_4,
+                   'impl_ext_4': diving.impl_ext_4,
+                   'impl_title_5': diving.impl_title_5,
+                   'impl_ext_5': diving.impl_ext_5
+                   }
+
+    return render(request, 'art-diving.html', diving_data)
+
 
 def contacts(request):
     return render(request, 'contacts.html')
 
 def naive_questions(request):
-    return render(request, 'naive-questions.html')
+    nq = Naive_questions.objects.get()
+    nq_data = {'name': nq.name,
+               'description': nq.description,
+               'about_title': nq.about_title,
+               'about_1': nq.about_1,
+               'about_2': nq.about_2,
+               'details_title': nq.details_title,
+               'details_1': nq.details_1,
+               'we_can': nq.we_can,
+               'we_can_1_title': nq.we_can_1_title,
+               'we_can_1': nq.we_can_1,
+               'we_can_2_title': nq.we_can_2_title,
+               'we_can_2': nq.we_can_2,
+               'we_can_3_title': nq.we_can_3_title,
+               'we_can_3': nq.we_can_3,
+               'impl_header': nq.impl_header,
+               'impl_title_1': nq.impl_title_1,
+               'impl_ext_1': nq.impl_ext_1,
+               'impl_title_2': nq.impl_title_2,
+               'impl_ext_2': nq.impl_ext_2,
+               'impl_title_3': nq.impl_title_3,
+               'impl_ext_3': nq.impl_ext_3
+               }
+    return render(request, 'naive-questions.html', nq_data)
 
 def portfolio(request):
     return render(request, 'portfolio.html')
